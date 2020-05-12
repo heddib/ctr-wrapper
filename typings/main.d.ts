329531978feb7f6e4980834c83951ab6ffce9a88
Kanna <heddi932@gmail.com>
2020-05-12 12:02:42 +0200

    First commit !

---
+++ typings/main.d.ts
@@ -0,0 +1,9 @@
+﻿import * as Request from "request-promise-native";
+declare class CTRAPIWrapper {
+    private BASE_URL;
+    private OPTIONS;
+    constructor(options?: Request.RequestPromiseOptions);
+    request(path: string, options?: Request.RequestPromiseOptions): Promise<any>;
+}
+export = CTRAPIWrapper;
+//# sourceMappingURL=main.d.ts.map
